9094ba7242ab47a2580865cc1193b06feab716b6
zhang yuyang <zhangyuyang1211@gmail.com>
2023-07-31 18:10:28 +0800

    Search Query

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,8 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+import SearchInput from "@/components/SearchInput";
 import React from "react";
 
 function RootPage() {
-  return <div></div>;
+  return (
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
+    </div>
+  );
 }
 
 export default RootPage;
