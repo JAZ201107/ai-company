9ef2171e3dcaabc18bbb6c6a244dc91c051576f7
zhang yuyang <zhangyuyang1211@gmail.com>
2023-07-31 17:55:32 +0800

    Sidebar Finished

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,11 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 function RootPage() {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default RootPage;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,6 +8,7 @@ import { UserButton } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { MobileSidebar } from "./mobile-sidebar";
 
 const font = Poppins({
   weight: "600",
@@ -18,9 +19,9 @@ interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b  border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b  border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden" />
+        <MobileSidebar />
         <Link href="/">
           <h1
             className={cn(
